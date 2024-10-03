b2f270bd33a5140dd8f650a5919e4c3d36e01f36
ukmothukuri <ukmothukuri@gmail.com>
2024-10-03 16:31:16 +0530

    added GC stuff

--- src/encapsulation/Main.java
+++ src/encapsulation/Main.java
@@ -4,10 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Person p1 = new Person();
-		p1.setPerson("XYZ", 25);
-		p1.getPerson();
+		Person p1 = new Person("Java", 29); // param cons
+		p1.getPerson(); // Java 29
+		
+		Person p2 = new Person(); // default
+		p2.getPerson(); // null 0
+		
+		p1 = null; // garbage
+		p2 = null;  // garbage
+		
+		new Person("Python",33).getPerson(); // 
 
 	}
-
 }
+
+
+// new Person()
+// GC => Automatic GC
--- src/encapsulation/Person.java
+++ src/encapsulation/Person.java
@@ -1,15 +1,22 @@
 package encapsulation;
 public class Person { // fully encapsulated class
-	private String name;
-	private int age;
+	private String name; // instance - Ref
+	private int age; // instance - prim
+	private static String country = "India"; // class - ref 
 	
-	public void setPerson(String n, int a) {
+	public Person() {
+		// instance - default values
+	}
+	
+	// eliminate the need of setter by a constructor
+	public Person(String n, int a) { // parameterized 
 		name = n;
 		age = a;
 	}
 	
 	public void getPerson() {
-		System.out.println(name+"\n"+age);
+		System.out.println(name+"\n"+age+"\n"+country);
 	}
-	
 }
+
+// constructor overloading
